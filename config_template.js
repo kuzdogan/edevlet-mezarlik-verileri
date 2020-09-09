f2bfd11fbb13bc9ff030090a611e69257046d935
kuzdogan <kaanuzdogan@hotmail.com>
2020-09-10 01:32:31 +0300

    Initial commit. Solve captchas, extract death count

---
+++ config_template.js
@@ -0,0 +1,6 @@
+// Fill the 2captcha API key and rename as config.js
+const config = {
+  twoCaptchaKey: ''
+}
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,151 @@
+const puppeteer = require('puppeteer');
+const CaptchaClient = require('@infosimples/node_two_captcha');
+const { twoCaptchaKey } = require('./config');
+const { URL } = require('url');
+const ora = require('ora');
+
+const captchaSolver = new CaptchaClient(twoCaptchaKey, {
+  timeout: 60000,
+  polling: 5000,
+  throwErrors: true
+});
+
+const CITIES = ['Istanbul', 'Konya', 'Bursa', 'Antalya', 'Erzurum', 'Diyarbakir', 'Kocaeli', 'Kahramanmaras', 'Malatya', 'Sakarya', 'Tekirdag'];
+const LINKS = {
+  'Istanbul': 'https://www.turkiye.gov.tr/istanbul-buyuksehir-belediyesi-vefat-sorgulama',
+  'Konya': 'https://www.turkiye.gov.tr/konya-buyuksehir-belediyesi-vefat-sorgulama',
+  'Bursa': 'https://www.turkiye.gov.tr/bursa-buyuksehir-belediyesi-vefat-sorgulama',
+  'Antalya': 'https://www.turkiye.gov.tr/antalya-buyuksehir-belediyesi-vefat-sorgulama',
+  'Erzurum': 'https://www.turkiye.gov.tr/erzurum-buyuksehir-belediyesi-vefat-sorgulama',
+  'Diyarbakir': 'https://www.turkiye.gov.tr/diyarbakir-buyuksehir-belediyesi-vefat-sorgulama',
+  'Kocaeli': 'https://www.turkiye.gov.tr/kocaeli-buyuksehir-belediyesi-vefat-sorgulama',
+  'Kahramanmaras': 'https://www.turkiye.gov.tr/kahramanmaras-buyuksehir-belediyesi-vefat-sorgulama',
+  'Malatya': 'https://www.turkiye.gov.tr/malatya-buyuksehir-belediyesi-vefat-sorgulama',
+  'Sakarya': 'https://www.turkiye.gov.tr/sakarya-buyuksehir-belediyesi-vefat-sorgulama',
+  'Tekirdag': 'https://www.turkiye.gov.tr/tekirdag-buyuksehir-belediyesi-vefat-sorgulama'
+};
+
+(async () => {
+
+  let eDevletURL = LINKS['Sakarya'];
+  // let eDevletURL = LINKS['Istanbul'];
+  console.log('Launching Browser');
+  const browser = await puppeteer.launch({ headless: false });
+  // const browser = await puppeteer.launch({ headless: true });
+
+  // Load page
+  console.log('Opened new page');
+  const page = await browser.newPage();
+
+  // Extract the CAPTCHA image.
+  let captchaBase64 = '';
+  page.on('response', async response => {
+    let url = new URL(response.url()); //WHATWH URL API
+    if (url.pathname === '/captcha') {
+      console.log('Received new Captcha');
+      const buffer = await response.buffer();
+      captchaBase64 = buffer.toString('base64');
+      // page.removeListener('response');
+    }
+  });
+
+  console.log('Going to the URL');
+  await page.goto(eDevletURL);
+  console.log('Loaded page');
+
+  let dateStr = '02/09/2018';
+  await enterDate(page, dateStr);
+  let id = await solveCaptchaAndSubmit(page, captchaSolver, captchaBase64);
+
+  // Until captcha is solved correctly, complain and submit again.
+  let trials = 0;
+  while (await isCaptchaError(page)) {
+    console.log('❌ Incorrect Captcha');
+    await captchaSolver.report(id); // Send complaint.
+    trials++;
+    await enterDate(page, dateStr);
+    id = await solveCaptchaAndSubmit(page, captchaSolver, captchaBase64);
+  }
+  console.log('Solved in ', trials, ' trials');
+
+  // Extract count. Check "Toplam X " element at the bottom of table. Or count rows.
+  let count = await page.$eval('#contentStart > div > div > div > span')
+    .then(elem => { // Found Toplam X element.
+      let countStr = elem.innerHTML.match(/(Toplam )(\d+)/)[2];
+      return parseInt(countStr);
+    })
+    .catch(() => {
+      // count table rows.
+      return page.$eval('#table > tbody', elem => {
+        return elem.children.length;
+      })
+    });
+  console.log('Total ', count, ' deaths found');
+  // await browser.close();
+})();
+
+
+
+
+async function enterDate(page, dateStr) {
+  console.log('Changing date to ' + dateStr)
+  await page.focus('#tarih');
+  await page.keyboard.down('Control');
+  await page.keyboard.press('A');
+  await page.keyboard.up('Control');
+  await page.keyboard.type(dateStr);
+  console.log('Changed date to ' + dateStr);
+}
+
+/**
+ * Function to have the captcha solved, submit the request, and have the page loaded.
+ * 
+ * @param {Object} page - Puppeteer page object
+ * @param {Object} client - Captcha Solver Client
+ * @param {String} captchaBase64 - Captcha as base64 String
+ * @returns {Promise} that resolves to the ID of the 2Captcha work. Used for wrong captcha complaints.
+ */
+async function solveCaptchaAndSubmit(page, client, captchaBase64) {
+  const spinner = ora({ spinner: 'dots12' }).start('Solving captcha ');
+  try {
+    let { text, id } = await solveCaptcha(client, captchaBase64);
+    spinner.stopAndPersist({ symbol: '✅', text: 'Solved captcha: ' + text + ' with work id:' + id });
+    await page.focus('#captcha_name');
+    await page.keyboard.type(text);
+    await page.focus('#mainForm > div > input.submitButton');
+    await page.keyboard.press('Space');
+    await page.waitForNavigation();
+    return id;
+  } catch (err) {
+    spinner.stopAndPersist({ symbol: '⚠️', text: 'Error solving captcha' });
+    console.log(err);
+    throw new Error('Error solving captcha. Timeout?')
+  }
+}
+
+function solveCaptcha(client, base64Image) {
+  return client.decode({
+    base64: base64Image,
+    min_len: 5,
+    max_len: 5,
+    numeric: 2
+  }).then((response => {
+    return response;
+  })).catch(console.error);
+};
+
+function sleep(sec) {
+  return new Promise(resolve => setTimeout(resolve, sec * 1000));
+}
+
+/**
+ * Function to check if CAPTCHA error is currently shown on the page
+ * 
+ * @param {Object} - page, Puppeteer page obj 
+ * @returns {Promise<Boolean>} that resolves to true if error is shown, false otherwise.
+ */
+function isCaptchaError(page) {
+  return page.$('#mainForm > fieldset > div.formRow.required.errored > div.fieldError')
+    .then(elem => elem !== null)
+    .catch(console.err);
+}
